3318e1fae5fc7cfbce370a9bdaa5a68e58b0fbf3
FieldsBarnett <fieldsbarnett@gmail.com>
2024-03-10 18:11:28 -0400

    change import order

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
+import './styles.css'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
-import './styles.css'
 
 // Set up a Router instance
 const router = createRouter({
