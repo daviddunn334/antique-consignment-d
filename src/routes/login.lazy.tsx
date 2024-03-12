ac58b06889641e1a1613f1e6be9c1dcdc0c4a8c6
FieldsBarnett <fieldsbarnett@gmail.com>
2024-03-12 00:31:52 -0400

    logging

--- src/routes/login.lazy.tsx
+++ src/routes/login.lazy.tsx
@@ -10,6 +10,7 @@ export const Route = createLazyFileRoute('/login')({
 function Login() {
     return (
         <>
+            <p>{import.meta.env.VITE_BASE_URL + "dashboard"}</p>
             <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-md">
                     <img
